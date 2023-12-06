103b289f893292c70d4bb7861cc84bc41bf810e8
wooJin <dldnwls0115@naver.com>
2023-12-06 10:37:33 +0900

    check

---
+++ app/src/test/java/Chicken.kt
@@ -0,0 +1,7 @@
+class Chicken(name:String, price:Int, val description : String): AbstractMenu(name, price){
+    override fun displayInfo() {
+        println("[$name] | W$price | $description")
+        println("위 메뉴를 장바구니에 추가하시겠습니까?")
+        println("1. 확인       2. 취소")
+    }
+}
