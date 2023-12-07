58c6ea75712c0b9b1d39fecb5c0aa86311752655
jericho31 <a@a.a>
2023-12-07 22:02:24 +0900

    Item의 info 구분자 줄 맞추고 코드 정리

--- app/src/main/java/com/example/kioskassignment/Item.kt
+++ app/src/main/java/com/example/kioskassignment/Item.kt
@@ -1,32 +1,11 @@
 package com.example.kioskassignment
 
 /** 각 메뉴 아이템 클래스. 부모클래스이지만 자식 분화 필요 없으면 이것만으로 사용해도 좋다. */
-open class Item {
-    var name = ""
-    var price = 0
-    var detail = ""
-
-    constructor(inName: String) {
-        name = inName
-    }
-
-    constructor(inName: String, inPrice: Int) {
-        name = inName
-        price = inPrice
-    }
-
-    constructor(inName: String, inPrice: Int, inDetail: String) {
-        name = inName
-        price = inPrice
-        detail = inDetail
-    }
-
-    /** 이름, 가격, 설명을 출력(println) */
-    fun displayInfo() {
-        println("$name\t| $price 원\t| $detail")
-    }
+open class Item(inName: String, inPrice: Int, inDetail: String) {
+    var name = inName
+    var price = inPrice
+    var detail = inDetail
 
     /** 이름, 가격, 설명을 String으로 반환 */
-    fun info(): String =
-        "$name\t| $price 원\t| $detail"
+    fun info(): String = "%-12s\t| %7s\t| %s".format(name, "${price}원", detail)
 }
